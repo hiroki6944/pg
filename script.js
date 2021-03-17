c3d19c226a22caa603aff1f1d13daf2d98ab85a9
hiroki6944 <79786023+hiroki6944@users.noreply.github.com>
2021-03-17 17:48:54 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+$(function(){
+
+    $('#btns').click(function(){
+        $('#signup-modal').fadeIn(500);
+
+       
+    });
+
+    $('.modal-close').click(function(){
+        $('#signup-modal').fadeOut(500);
+    });
+
+
+    $('#btnes').click(function(){
+        $('#login-modal').fadeIn(500);
+    });
+
+    $('.modal-close').click(function(){
+        $('#login-modal').fadeOut(500);
+    });
+
+    
+
+   
+
+
+    
+});
